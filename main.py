96f7f776ab7b295b502bb81b8483619f85409597
Bohdan Yakovenko <119360642+BohdanYakovenko@users.noreply.github.com>
2022-12-06 21:37:25 +0200

    Update main.py
    Asking player's name

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
+players_name = input("Enter your name: ")
 print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
 
